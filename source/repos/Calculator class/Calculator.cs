864a0090406dc553e409d48bbd900803800e6c8b
Florence-wangui <wanguif018@gmail.com>
2022-05-04 22:01:14 +0300

    first commit

---
+++ source/repos/Calculator class/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_class
+{
+    public class Calculator {}
+    
+    public class CalCulator 
+    {
+        // Square function   
+        public int Square(int num)
+        {
+            return num * num;
+        }
+       
+        // Add two integers and returns the sum  
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Multiply two integers and retuns the result  
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        // Subtracts small number from big number  
+        public int Subtract(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+
+            return num2 - num1;
+
+        }
+        //performing Division on two float variables.  
+        public float Division(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ source/repos/Calculator class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                CalCulator sq = new CalCulator();
+                Console.WriteLine(sq.Square(12));
+                Console.WriteLine(sq.Add(8, 63));
+                Console.WriteLine(sq.Multiply(5, 8));
+                Console.WriteLine(sq.Subtract(10, 42));
+                Console.WriteLine(sq.Division(3, 4));
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
